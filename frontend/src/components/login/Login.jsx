c4cbd3d6597b69bcc5390750032bf09f1ce9797d
yuehtingchen <alicetingchen@gmail.com>
2022-02-26 22:21:46 -0500

    add basic template for frontend

---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Login extends React.Component{
+    render() {
+        return (
+            <div>
+                <p>hello</p>
+            </div>
+        )
+    }
+}
+
