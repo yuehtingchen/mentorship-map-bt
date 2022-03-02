8c1cfbc23efa7f71088ea35924260a4e68efed72
jthsu2001 <roy.h9222001@gmail.com>
2022-03-01 21:13:36 -0500

    welcome page draft

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import "./Login.css"
 
 export default class Login extends React.Component{
     render() {
         return (
-            <div>
-                <p>hello</p>
+            <div className='page'>
+                <div class="split left">
+                    <div class = 'center'>
+                        <img src = "./logo.png" alt = "logo"></img>
+                    </div>
+                </div>
+
+                <div class="split right">
+                    <h5>Welcome!</h5>
+                    <div class="buttons">
+                        <button>Login</button>
+                    </div>
+                </div>  
             </div>
         )
     }
