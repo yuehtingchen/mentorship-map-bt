d2e65c556b4a487bb02cbac645913d9fb09a6b5b
yuehtingchen <alicetingchen@gmail.com>
2022-03-20 22:02:20 -0400

    add google login button

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import "./Login.css"
 import logo from './logo.png';
+import GoogleLogin from 'react-google-login';
+import axios from 'axios';
 
 export default class Login extends React.Component{
+    
+    onGoogleLoginSuccess = (response) => {
+        const idToken = response.tokenId;
+        const data = {
+            email: response.profileObj.email,
+            first_name: response.profileObj.givenName,
+            last_name: response.profileObj.familyName
+        };
+    
+        this.validateTokenAndObtainSession({ data, idToken })
+            .then()
+            .catch();
+    }
+
+    onGoogleLoginFailure = (response) => {
+        
+    }
+
+    validateTokenAndObtainSession({data, idToken}) {
+        const headers = {
+            Authorization: idToken,
+            'Contend-Type': 'application/json'
+        };
+
+        // Need to change the hardcoded url
+        axios.post('http://127.0.0.1:8000//user/login', data, { headers });
+    }
     render() {
         return (
             <div className='page'>
@@ -15,11 +44,18 @@ export default class Login extends React.Component{
                 <div class="split right">
                     <div class = "center">
                         <h5>Welcome!</h5>
-                        <button>Login</button>
+                        <GoogleLogin
+                            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}  // your Google app client ID
+                            buttonText="Sign in with Google"
+                            onSuccess={this.onGoogleLoginSuccess} // perform your user logic here
+                            onFailure={this.onGoogleLoginFailure} // handle errors here
+                            isSignedIn={true}
+                        />
                     </div>
                 </div>  
             </div>
         )
-    }
-}
+    }  
 
+ 
+}
