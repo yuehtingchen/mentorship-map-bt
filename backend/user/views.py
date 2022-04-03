ca0bc51f2c6cad37421435783c1f087180d5bfd4
yuehtingchen <alicetingchen@gmail.com>
2022-04-02 20:43:17 -0400

    create user session

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,9 +1,14 @@
+from ast import Raise
+from multiprocessing import AuthenticationError
 from xml.dom import ValidationErr
+from xmlrpc.client import ResponseError
 from django.conf import settings
+from django.contrib.auth import login
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
-from rest_framework import viewsets, serializers
+from rest_framework import viewsets, exceptions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -11,10 +16,8 @@ from rest_framework.permissions import AllowAny
 from .serializers import UsersSerializer
 from .models import Users
 
-# from api.mixins import ApiErrorsMixin, PublicApiMixin
 from google.oauth2 import id_token
 from google.auth.transport import requests
-# from .jwt import jwt_login
 
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = Users.objects.all().order_by('id')
@@ -26,22 +29,23 @@ class UserLoginAPI(APIView):
     def post(self, request, *args, **kwargs):
         token = request.headers.get('Authorization')
 
-        # (Receive token by HTTPS POST)
         try:
             # Specify the CLIENT_ID of the app that accesses the backend:
             idinfo = id_token.verify_oauth2_token(token, requests.Request(), settings.GOOGLE_OAUTH2_CLIENT_ID)
-
             # ID token is valid. Get the user's Google Account ID from the decoded token.
-            userid = idinfo['sub']
+            user_email = idinfo['email']
+            
         except ValueError:
             # Invalid token
-            raise ValidationErr("Invalid Token")
+            raise exceptions.AuthenticationFailed("Invalid Token")
         
-        serializer = self.UserSerializer(data=request.data)
+        serializer = UsersSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # user,_ = user_get(**serializer.validated_data)
+        
+        try:
+            user = Users.objects.get(email=user_email)
+        except ObjectDoesNotExist:
+            raise exceptions.AuthenticationFailed("User does not exist")
 
-        # response = Response(data=user_get_me(user=user))
-        # response = jwt_login(response=response, user=user)
-
-        return Response(data=serializer.data)
+        login(request, user)
+        return Response(data=user)
