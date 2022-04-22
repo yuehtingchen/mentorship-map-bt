2c9e148b843a97feea40dfbb267fd8ba43c6ecc3
Andrew1207-Yang <a.yang353188@gmail.com>
2022-04-22 14:47:43 +0800

    add localhost postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Mentorship',
+        'USER': "postgres",
+        'PASSWORD' : "andrew1207",
+        "HOST" : "localhost",
+        "PORT" : '5432'
     }
 }
 
