1f4a6416642b0d8c1dafab01a0793c85db73be11
yuehtingchen <alicetingchen@gmail.com>
2022-04-23 18:53:41 -0400

    remove header component and add bootstrap header

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./Map.css"
 import logo from './logo.png';
+import { Navbar, Nav, Container} from 'react-bootstrap';
 
 export default class Map extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div>
+                <Navbar className="navbar">
+                <Container>
+                    <Navbar.Brand href="#home"><span id="brand">Mentor Map</span></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#link"><span id="link">Map</span></Nav.Link>
+                        <Nav.Link href="#link"><span id="link">Database</span></Nav.Link>
+                        <Nav.Link href="#link"><span id="link">Logout</span></Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+                </Navbar>
                 <div className="map"></div>
                 <footer>
                     <a>About</a>
