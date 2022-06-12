707aa956d3b617b4f0d1da602113c87e49c4914e
yuehtingchen <alicetingchen@gmail.com>
2022-06-11 18:09:34 -0700

    add google maps

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 import "./Map.css"
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import logo from './logo.png';
-import { Navbar, Nav, Container} from 'react-bootstrap';
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import Header from "../Header.jsx"
+
+const containerStyle = {
+    width: "100%",
+    height: "700px"
+};
+    
+const center = {
+    lat: 37.0902,
+    lng: -95.7129
+};
 
 export default class Map extends React.Component{
     constructor(props) {
         super(props);
-        /* ref = React.useRef(null);
-        [map, setMap] = React.useState();
-
-        React.useEffect(() => {
-            if (ref.current && !map) {
-                setMap(new window.google.maps.Map(ref.current, {center: { lat: -34.397, lng: 150.644 }, zoom: 6,}));
-            }
-        }, [ref, map]); */
     }
+
 
     render() {
         return (
             <div>
-                <Navbar className="navbar">
-                <Container>
-                    <Navbar.Brand href="#home"><span id="brand">Mentor Map</span></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#link"><span id="link">Map</span></Nav.Link>
-                        <Nav.Link href="/database"><span id="link">Database</span></Nav.Link>
-                        <Nav.Link href="#link"><span id="link">Logout</span></Nav.Link>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-                </Navbar>
-                <div className="map">
-
-                </div>
+                <Header/>
+                <LoadScript
+                    googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API}
+                    >
+                    <GoogleMap
+                        mapContainerStyle={containerStyle}
+                        center={center}
+                        zoom={4}
+                    >
+                        { /* Child components, such as markers, info windows, etc. */ }
+                        <></>
+                </GoogleMap>
+                </LoadScript>
                 <footer>
                     <a>About</a>
                     <a>Instagram</a>
