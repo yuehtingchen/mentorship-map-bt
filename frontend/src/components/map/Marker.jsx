2050f4efae7767147126962cd25492bedcea43f1
yuehtingchen <alicetingchen@gmail.com>
2022-06-26 16:10:06 -0700

    add marker formatting

--- frontend/src/components/map/Marker.jsx
+++ frontend/src/components/map/Marker.jsx
@@ -3,6 +3,9 @@ import API from "../../api/Api.jsx";
 import "./Marker.css";
 import { Marker, InfoWindow } from '@react-google-maps/api';
 import flag from "../../img/flag.png";
+import check from "../../img/check.png";
+import deleteImg from "../../img/delete.png";
+import question from "../../img/question.png";
 
 export default class MyMarker extends React.Component {
     constructor(props) {
@@ -30,7 +33,8 @@ export default class MyMarker extends React.Component {
                     name: res.name, 
                     city: res.city,
                     country: res.country,
-                    mentors: res.mentors
+                    mentors: res.mentors,
+                    essay_editing: null,
                 });
             });
         }
@@ -41,11 +45,19 @@ export default class MyMarker extends React.Component {
         let mentorContent = []
         for(const mentor of mentors) {
             mentorContent.push((
-                <div key={mentor.email} className="markerContent">
-                    <strong>{`${mentor.first_name} ${mentor.last_name}`} </strong>
-                    <br/>
-                    {`Class of ${mentor.grad_year}`} <br/>
-                    {mentor.intro? <p>{mentor.intro}</p>: null}
+                <div key={mentor.email} className="mentorConent">
+                    <div className="mentorHeader">
+                    <strong className="mentorName">{`${mentor.first_name} ${mentor.last_name}`} </strong>
+                    <br/><br/>
+                    {`${this.state.name}, ${mentor.major}, ${mentor.grad_year}`} 
+                    </div>
+
+                    {mentor.intro? <p>{mentor.intro}</p>: null }
+
+                    Essay Editing <img src={
+                        mentor.essay_editing == null ? question : 
+                        (mentor.essay_editing ? check : deleteImg)}></img> <br/><br/>
+                    Contact: <br/>
                     <a href={`mailto: ${mentor.email}`}>{mentor.email}</a>
                     <br/>
                     <a href={mentor.linkedin}>{mentor.linkedin}</a>
@@ -61,14 +73,19 @@ export default class MyMarker extends React.Component {
                 {this.props.isOpen && <InfoWindow
                     onCloseClick={this.onToggleOpen}
                     options={{ closeBoxURL: ``, enableEventPropagation: true }}
+                    className="infoWindow"
                 >
-                    <div style={{ backgroundColor: `white`, opacity: 0.9, padding: `12px` }}>
-                    <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
-                        <h5>{`${this.state.name}`}</h5>
-                        <div>{`${this.state.city}, ${this.state.country}`}</div>
+                    <div>
+                        <div className="header">
+                        <h4>{`${this.state.name}`}</h4>
+                        <div>{`${this.state.city} | ${this.state.country}`}</div>
+                        </div>
+
+                        <div className="content">
                         {mentorContent}
+                        </div>
                     </div>
-                    </div>
+                   
                 </InfoWindow>}
             </Marker>
         )
